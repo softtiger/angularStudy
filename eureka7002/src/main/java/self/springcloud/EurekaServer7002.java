ddb2993a9f377120d8dd6f11fac40e5cc5fbb5c2
softtiger <revokeyu@hotmail.com>
2020-06-23 16:12:37 +0800

    Eureka  Cluster

---
+++ eureka7002/src/main/java/self/springcloud/EurekaServer7002.java
@@ -0,0 +1,11 @@
+package self.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer7002 {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaServer7002.class,args);
+    }
+}
