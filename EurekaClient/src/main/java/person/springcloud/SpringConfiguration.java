1b6c5205f5556dbb97bd69153f74c8d71d18ebc9
softtiger <revokeyu@hotmail.com>
2020-06-29 21:36:23 +0800

    use ribbion as loadbalance

--- EurekaClient/src/main/java/person/springcloud/SpringConfiguration.java
+++ EurekaClient/src/main/java/person/springcloud/SpringConfiguration.java
@@ -1,6 +1,7 @@
 package person.springcloud;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class SpringConfiguration {
 
     @Bean
+    @LoadBalanced
     RestTemplate   getRestTemplate(){
          return new RestTemplate();
     }
--- EurekaClient/src/main/java/person/springcloud/controller/ClientController.java
+++ EurekaClient/src/main/java/person/springcloud/controller/ClientController.java
@@ -3,6 +3,7 @@ package person.springcloud.controller;
 import com.infras.ResultJSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,9 +13,11 @@ public class ClientController {
     @Autowired
     private RestTemplate restTemplate;
 
-    private String url = "http://localhost:8001/product/list";
+   // private String url = "http://localhost:8001/product/list";
+   private String url = "http://PRODUCT-SERVICE/product/list";
 
     @RequestMapping("/product/list")
+    @ResponseBody
     public ResultJSONObject onlineProductList(){
              return restTemplate.getForObject(url, ResultJSONObject.class);
     }
